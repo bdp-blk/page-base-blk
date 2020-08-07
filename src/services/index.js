cd419bcbb4ee50d7f62963c38e96f03b2c2bb6db
samy <samyzhg@qq.com>
2020-08-07 12:47:17 +0800

    修改model和sevice

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,33 @@
 /**
  * @author samy
  * @email samyzhg#qq.com
- * @create date 2020-07-17 10:04:59
- * @modify date 2020-07-17 10:04:59
- * @desc service示例
+ * @create date 2020-08-06 21:00:24
+ * @modify date 2020-08-06 21:00:24
+ * @desc 接口地址示范
  */
 import request from '@/bdpcloud/utils/request';
+import { delayPms } from '@<%=proName%>/utils/utils';
 
-export async function <%=method%>(params) {
-  return request('smartweb/smartservice/<%=method%>', {
+import { getListM, getTreeM } from './mock';
+const isMock = true;
+
+// 首页api列表
+export async function getList(params) {
+  if (isMock) {
+    return delayPms(getListM);
+  }
+  return request('daweb/getList', {
+    method: 'POST',
+    body: params,
+  });
+}
+
+// 首页目录菜单
+export async function getTree(params) {
+  if (isMock) {
+    return delayPms(getTreeM);
+  }
+  return request('daweb/getTree', {
     method: 'POST',
     body: params,
   });
