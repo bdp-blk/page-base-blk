fca3381f621fbbd4f79b4adffb533dc141f80b70
samy <yessz@foxmail.com>
2020-12-26 20:13:40 +0800

    修改组件引入方式

--- src/components/index.js
+++ src/components/index.js
@@ -3,15 +3,12 @@
  * @email: yessz#foxmail.com
  * @time: 2020-12-24 20:35:27
  * @modAuthor: samy
- * @modTime: 2020-12-25 19:41:18
+ * @modTime: 2020-12-26 20:04:36
  * @desc: 导出
  * @Copyright © 2015~2020 BDP FE
  */
-import { Base, BaseSub } from '@<%=proName%>/base';
 import DetailModal from "./DetailModal";
 
-export { Base, BaseSub };
-export { MyIcon, CategoryTree, ExpandTable, CommonFilter, BatchButton, CommonModal } from '@<%=proName%>/components';
-
+export * from '@<%=proName%>/components';
 export { DetailModal };
 
